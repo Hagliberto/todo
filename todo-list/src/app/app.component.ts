0c73081e32e687aae9e120d5a1df276bf79aa07e
Hagliberto <hagliberto@gmail.com>
2024-08-03 11:02:34 -0300

    Gerenciador da lista de tarefas

--- todo-list/src/app/app.component.ts
+++ todo-list/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TodoComponent } from './todo/todo.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [RouterOutlet, TodoComponent]
 })
 export class AppComponent {
   title = 'todo-list';
