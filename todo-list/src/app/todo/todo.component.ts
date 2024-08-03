c3a1bd01cd51ff27679b3ab490528c90574c4ed6
Hagliberto <hagliberto@gmail.com>
2024-08-03 10:28:22 -0300

    Gerenciador da lista de tarefas

--- todo-list/src/app/todo/todo.component.ts
+++ todo-list/src/app/todo/todo.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 
+interface Task {
+  name: string;
+  completed: boolean;
+}
+
 @Component({
   selector: 'app-todo',
-  standalone: true,
-  imports: [],
   templateUrl: './todo.component.html',
-  styleUrl: './todo.component.scss'
+  styleUrls: ['./todo.component.css']
 })
 export class TodoComponent {
+  newTask: string = '';
+  tasks: Task[] = [];
 
+  addTask() {
+    if (this.newTask.trim()) {
+      this.tasks.push({ name: this.newTask, completed: false });
+      this.newTask = '';
+    }
+  }
+
+  toggleTask(index: number) {
+    this.tasks[index].completed = !this.tasks[index].completed;
+  }
+
+  removeTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
 }
